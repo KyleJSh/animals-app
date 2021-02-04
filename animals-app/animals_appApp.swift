09ac1519b3915f606c60908ca7b0979b4f347ec2
Kyle Sherrington <kylejsherrington@gmail.com>
2021-02-03 19:18:56 -0800

    Initial Commit

---
+++ animals-app/animals_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  animals_appApp.swift
+//  animals-app
+//
+//  Created by Kyle Sherrington on 2021-02-03.
+//
+
+import SwiftUI
+
+@main
+struct animals_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
